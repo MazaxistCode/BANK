9f28576cd0c8b1094a608951f53071224d82a9d8
lisav <lisav@DESKTOP-91LHVVB>
2025-04-22 15:03:11 +0300

    Добавьте файлы проекта.

---
+++ BANK/Report.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BANK
+{
+    /// <summary>
+    /// Логика взаимодействия для Report.xaml
+    /// </summary>
+    public partial class Report : Window
+    {
+        public Report()
+        {
+            InitializeComponent();
+            DataTransaction.ItemsSource = DB.BANKEntities.GetContext().Financial_transactions.ToList();
+        }
+        public void UpdateDB()
+        {
+            List<DB.Financial_transactions> financial_Transactions = new List<DB.Financial_transactions>();
+            if ((bool)WaneBox.IsChecked)
+                financial_Transactions.AddRange(DB.BANKEntities.GetContext().Financial_transactions.Where(finTrans => finTrans.Money <= 0));
+            if ((bool)(ProfitBox != null ? ProfitBox.IsChecked : true))
+                financial_Transactions.AddRange(DB.BANKEntities.GetContext().Financial_transactions.Where(finTrans => finTrans.Money > 0));
+            DataTransaction.ItemsSource = financial_Transactions;
+        }
+        private void WaneBox_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateDB();
+        }
+
+        private void ProfitBox_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateDB();
+        }
+
+        private void ProfitBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            UpdateDB();
+        }
+
+        private void WaneBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            UpdateDB();
+        }
+    }
+}
---
+++ BANK/Transaction.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BANK
+{
+    /// <summary>
+    /// Логика взаимодействия для Transaction.xaml
+    /// </summary>
+    public partial class Transaction : Window
+    {
+        public Transaction()
+        {
+            InitializeComponent();
+            DataTransaction.ItemsSource = DB.BANKEntities.GetContext().Financial_transactions.ToList();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(string.IsNullOrEmpty(NameBox.Text) && string.IsNullOrEmpty(MoneyBox.Text)) && Calendar.SelectedDate != null)
+            {
+                DB.BANKEntities.GetContext().Financial_transactions.Add(new DB.Financial_transactions()
+                {
+                    Date = (DateTime)Calendar.SelectedDate,
+                    Name = NameBox.Text,
+                    Money = int.TryParse(MoneyBox.Text, out int result) ? result : 0
+                });
+                DB.BANKEntities.GetContext().SaveChanges();
+                DataTransaction.ItemsSource = DB.BANKEntities.GetContext().Financial_transactions.ToList();
+            }
+            else
+                MessageBox.Show("Заполните все поля");
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(string.IsNullOrEmpty(NameBox.Text) && string.IsNullOrEmpty(MoneyBox.Text)) && Calendar.SelectedDate != null && DataTransaction.SelectedItem is DB.Financial_transactions)
+            {
+                var item = DataTransaction.SelectedItem as DB.Financial_transactions;
+                item.Date = (DateTime)Calendar.SelectedDate;
+                item.Name = NameBox.Text;
+                item.Money = int.TryParse(MoneyBox.Text, out int result) ? result : 0;
+                DB.BANKEntities.GetContext().SaveChanges();
+                DataTransaction.ItemsSource = DB.BANKEntities.GetContext().Financial_transactions.ToList();
+            }
+            else
+                MessageBox.Show("Заполните все поля и выберите транзакцию");
+        }
+
+        private void RemButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataTransaction.SelectedItem is DB.Financial_transactions)
+            {
+                DB.BANKEntities.GetContext().Financial_transactions.Remove(DataTransaction.SelectedItem as DB.Financial_transactions);
+                DB.BANKEntities.GetContext().SaveChanges();
+                DataTransaction.ItemsSource = DB.BANKEntities.GetContext().Financial_transactions.ToList();
+            }
+            else
+                MessageBox.Show("Выберите транзакцию");
+        }
+
+        private void DataTransaction_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            if (DataTransaction.SelectedItem is DB.Financial_transactions)
+            {
+                var item = DataTransaction.SelectedItem as DB.Financial_transactions;
+                MoneyBox.Text = item.Money.ToString();
+                NameBox.Text = item.Name;
+                Calendar.DisplayDate = item.Date;
+            }
+        }
+    }
+}
